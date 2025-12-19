707e9ae013fa73380cc741f723c097ae0186db8a
James Rossini <james.rossini@posteo.de>
2025-12-19 09:09:14 +0100

    - Updated POM file and module info: New dependency: "API Specialized Entities"
    Cf. sister module, commit b11b30a8d047f382e4795ae876b5c19b25c97ab1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,19 @@
 module kmymoney.apiext {
+
 	requires static org.slf4j;
 	requires java.desktop;
 	
-	requires transitive schnorxoborx.schnorxolib;
+	// ----------------------------
 
+	requires transitive schnorxoborx.schnorxolib;
+	
 	requires transitive kmymoney.base;
 	requires transitive kmymoney.api;
+	requires transitive kmymoney.apispec;
+
+	// ----------------------------
 
 	exports org.kmymoney.apiext.secacct;
 	exports org.kmymoney.apiext.trxmgr;
+
 }
